a748987428fb0bfea8443c0c64354103c644e8f5
A V <>
2021-05-02 03:59:15 +0000

    Help command: Sorted output commands

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -20,7 +20,7 @@ module.exports = {
 
     if (!args.length) {
       data.push("Here's a list of all my commands:");
-      data.push(commands.map((command) => command.name).join(", "));
+      data.push(commands.sort((a, b) => a.name > b.name ? 1 : -1).map((command) => command.name).join(", "));
       data.push(
         `\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`
       );
