aebb1d2751237e9aa5383851910bc7914176b283
A V <altivu@gmail.com>
2021-06-28 21:28:28 +0000

    Added some item mode resources to tutorial command

--- commands/links/tutorial.js
+++ commands/links/tutorial.js
@@ -1,11 +1,19 @@
 module.exports = {
 	name: 'tutorial',
-  aliases: ['tutorials', 'drift'],
-	description: 'Provides some links to basic drifting guides.',
+  aliases: ['tutorials'],
+	description: 'Provides some links to KartRider Rush+ guides.',
   result(_client, message, args, embed) {
-    embed.setTitle("Drifting Tutorials")
-    .setDescription(`[Drifting 101 (by phreaky)](https://www.youtube.com/watch?v=YJuaIdPFnB4)
-    [The Road to Mastering Drag Drifts series (by 幸板砖, translated by MadCarroT)](https://www.youtube.com/watch?v=hndh4E_kbDw&list=PL3lisfoN_cJWbhh6pczZjo74HNzZHgyrG&index=1)`)
+    embed.setTitle("Tutorials")
+    .addFields({
+      name: "Speed",
+      value: `[Drifting 101 (video by phreaky)](https://www.youtube.com/watch?v=YJuaIdPFnB4)
+[The Road to Mastering Drag Drifts (video series by 幸板砖, translated by MadCarroT)](https://www.youtube.com/watch?v=hndh4E_kbDw&list=PL3lisfoN_cJWbhh6pczZjo74HNzZHgyrG&index=1)`
+    })
+    .addFields({
+      name: "Item",
+      value: `[KartRider Item Race Strategies (Google Doc by shrubin & RLAgent)](https://docs.google.com/document/d/1OpNAe-TnbQQXE0U_aWogD3QAM5prftJ7SDAoMXedGtM/edit#heading=h.reqw6izic7gv)
+[Item Guide for Beginners (video by The Crown)](https://www.youtube.com/watch?v=Gi8hAjNkro4)`
+    });
 
     return embed;
   }
