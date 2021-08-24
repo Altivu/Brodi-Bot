994a32ac01ecb21a718d63d99d7e0e27ba43d822
A V <altivu@gmail.com>
2021-08-24 04:46:28 +0000

    records: Updated link

--- commands/links/records.js
+++ commands/links/records.js
@@ -1,9 +1,11 @@
 module.exports = {
 	name: 'records',
-	description: 'Provides the link to the Rush+ Records List.',
+	description: 'Provides the link to the Global Server Records sheet.',
   result(_client, message, args, embed) {
-    embed.setTitle("Rush+ Records List")
-    .setDescription("[International Server Records compiled by PF_Horace](https://docs.qq.com/doc/DTEFsSWxNZlhJQW1E)")
+    embed.setTitle("Global Server Records")
+    .setDescription(`[글로벌 타임어택 기록 國際服計時榜 (Global Server Records) compiled by PF_Horace/Run Wildly 狂熱跑跑/DMS](https://docs.google.com/spreadsheets/u/1/d/1yQY45eWh3Dc7pxlCqf-79rXjPJ1nXrCbGnzKQcYFu6s/htmlview#)
+
+(Note that the sheet is in Korean/Chinese)`)
 
     return embed;
   }
