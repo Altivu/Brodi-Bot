7f0ed121bd357bb0229a4dc90dba5a823d4058fd
A V <altivu@gmail.com>
2022-04-09 13:47:44 -0400

    Minor spacing changes

--- auth.js
+++ auth.js
@@ -32,11 +32,13 @@ let oAuth2Client;
  */
 exports.authorize = () => {
   const { client_secret, client_id, redirect_uris } = credentials.installed;
+
   oAuth2Client = new google.auth.OAuth2(
     client_id,
     client_secret,
     redirect_uris[0]
   );
+  
   oAuth2Client.setCredentials(token);
 
   console.log("Authorized to Google API.")
